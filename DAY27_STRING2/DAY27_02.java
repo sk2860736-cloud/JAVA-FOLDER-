e7627ef6f141ad61f81253b96410423b92f00ea3
sk2860736-cloud <sk2860736@gmail.com>
2026-04-11 19:28:44 +0530

    Add files via upload

---
+++ DAY27_STRING2/DAY27_02.java
@@ -0,0 +1,87 @@
+package DAY27_STRING2;
+
+public class DAY27_02 {
+    public static void main(String[] args) {
+        // String Method --> 
+
+        // length/Emptiness --> 
+
+        // String s1 = new String("Sachin Gupta");
+        // // System.out.println(s1.length()); //12
+        // System.out.println(s1.isEmpty()); //false 
+        // System.out.println(s1.isBlank()); // false 
+
+        // Character Access --> 
+        // String s2 = new String("Sachin");
+
+        // System.out.println(s2.toCharArray()); // Sachin
+        // System.out.println(s2.charAt(5)); // n 
+
+        // Searchin --> 
+        // String s1 = new String("Sachin");
+        // String s2 = new String("a"); 
+
+        // System.out.println(s1.contains(s2)); // false \
+
+        // String s1 = new String("Sachin");
+        // System.out.println(s1.indexOf('n'));
+
+        // String s1 = new String("Sachin Gupta");
+        // System.out.println(s1.lastIndexOf('a')); //11
+
+        // String s1 = new String("Sachin Gupta");
+        // System.out.println(s1.startsWith("Gupta",7));  // true 
+
+        //   String s2 = new String("Sachin Gupta" );
+        // System.out.println(s2.endsWith("Gupta")); // true 
+
+        // Extraction/ Transformation --> 
+
+        // String s1 = new String("Sachin Gupta");
+        // System.out.println(s1.substring(7,9)); // start with Gu
+
+        // String s1 = new String("Sachin Gupta");
+        // System.out.println(s1.toUpperCase()); // SACHIN GUPTA 
+
+        // String s2 = new String("SACHIN GUPTA");
+        // System.out.println(s2.toLowerCase());
+
+        // String s1 = new String("  Sachin Gupta    "); //spaces remove but not mid space 
+        // System.out.println(s1.trim());
+
+        
+        // String s2 = new String("Sachin Gupta  "); 
+        // System.out.println(s2.strip()); // unicode remove in java 11
+
+        // String s1 = new String("Sachin Gupta ");
+        // System.out.println(s1.repeat(5));
+
+        // String s2 = new String("Sachin Gupta");
+        // System.out.println(s2.replace("Sachin Gupta", "Sufaid Hussain"));
+
+        
+        // String s2 = new String("Sachin");
+        // System.out.println(s2.replaceAll("Sachin", "Sufaid"));
+
+        // String s2 = new String("Sachin,Deepak,Rajmohammad");
+        // String []arr = s2.split(",");
+
+        // for(String s: arr){
+        //     System.out.println(s); 
+        // }
+       
+
+        // String s3 = new String.join("-","A",);
+        // System.out.println(s3.);
+
+
+    //    String result = String.join("-", "A", "B", "C");
+    //    System.out.println(result);
+
+
+
+    // String s1 = new String ("Sachin Gupta");
+    // System.out.println(s1.valueOf());
+    }
+    
+}
